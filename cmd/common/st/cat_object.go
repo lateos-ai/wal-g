cf1785513cda9b7acd10b0fdeabecbd34f2aae1a
Leo <your-email@example.com>
2026-06-06 10:04:49 -0700

    fix: add blank line between import groups to satisfy gci

--- cmd/common/st/cat_object.go
+++ cmd/common/st/cat_object.go
@@ -3,6 +3,7 @@ package st
 import (
 	"github.com/spf13/cobra"
 	"github.com/wal-g/tracelog"
+
 	"github.com/lateos-ai/wal-g/internal/multistorage/exec"
 	"github.com/lateos-ai/wal-g/internal/storagetools"
 	"github.com/lateos-ai/wal-g/pkg/storages/storage"
