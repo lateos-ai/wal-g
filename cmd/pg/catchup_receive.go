631495cf13f2e327259c21bae045ba6e6a72280b
Leo <your-email@example.com>
2026-06-06 20:08:03 -0700

    fix: gci import formatting in cmd/pg (catchup_send, catchup_receive, catchup_push)

--- cmd/pg/catchup_receive.go
+++ cmd/pg/catchup_receive.go
@@ -3,10 +3,9 @@ package pg
 import (
 	"strconv"
 
-	"github.com/wal-g/tracelog"
-	"github.com/spf13/cobra"
-
 	"github.com/lateos-ai/wal-g/internal/databases/postgres"
+	"github.com/spf13/cobra"
+	"github.com/wal-g/tracelog"
 )
 
 const (
